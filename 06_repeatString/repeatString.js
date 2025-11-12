405c4533b2c515e7e54fafbc42eedbbcafc85fb6
Daniel Elbert <208325543+DirtyDan-77@users.noreply.github.com>
2025-11-11 20:00:00 -0800

    feat: complete javascript exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+function repeatString(string, num) {
+  if (num < 0) return 'ERROR';
+  
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += string; // simpler and clearer than concat
+  }
+  return result;
+}
 
-};
-
-// Do not edit below this line
 module.exports = repeatString;
