7e4ccfd5631f29f032cf361e33c69ffcabc7ef59
Daniel Elbert <208325543+DirtyDan-77@users.noreply.github.com>
2025-11-11 20:06:03 -0800

    feat: compelte javascript exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    newStr = str.split("");
+    newStr = newStr.reverse();
+    return newStr = newStr.join("");
 };
 
 // Do not edit below this line
