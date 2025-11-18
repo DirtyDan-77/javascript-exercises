b63ae2f811cd2838c1663b3660fabcb44e8f1e12
Daniel Elbert <208325543+DirtyDan-77@users.noreply.github.com>
2025-11-17 17:29:53 -0800

    feat: fail problem 10 of javascript

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // A year is a leap year if:
+    // 1. It is divisible by 4
+    // 2. But NOT divisible by 100
+    // 3. Unless it IS divisible by 400
 
+    if (year % 400 === 0) {
+        return true;
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    return year % 4 === 0;
 };
 
 // Do not edit below this line
