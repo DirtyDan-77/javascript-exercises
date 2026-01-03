c3885d7e8d2490ebe97241b512782256a9aae489
Daniel Elbert <208325543+DirtyDan-77@users.noreply.github.com>
2026-01-03 15:12:49 -0800

    feat: complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  if (typeof numOne !== "number" || typeof numTwo !== "number") {
+    return "invalid input";
+  }
+  return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+  if (typeof numOne !== "number" || typeof numTwo !== "number") {
+    return "invalid input";
+  }
+  return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (!Array.isArray(arr)) return "invalid input";
+
+  return arr.reduce((sum, num) => {
+    if (typeof num !== "number") return sum;
+    return sum + num;
+  }, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (!Array.isArray(arr)) return "invalid input";
 
+  return arr.reduce((mul, num) => {
+    if (typeof num !== "number") return mul;
+    return mul * num;
+  }, 1);
 };
 
-const power = function() {
-	
+
+const power = function(numOne, numTwo) {
+  if (typeof numOne !== "number" || typeof numTwo !== "number") {
+    return "invalid input";
+  }
+  return numOne ** numTwo;
 };
 
-const factorial = function() {
-	
+
+const factorial = function (num) {
+  if (typeof num !== "number" || num < 0) return "invalid input";
+  if (num === 0 || num === 1) return 1;
+
+  let count = num;
+  for (let i = num - 1; i > 0; i--) {
+    count *= i;
+  }
+
+  return count;
 };
 
 // Do not edit below this line
